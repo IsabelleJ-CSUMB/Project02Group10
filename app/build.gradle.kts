c8f0b5a1549b60b202ad4e0b7bce1467b48af576
oscar101258 <ogonzalezcuevas@csumb.edu>
2026-04-27 20:41:51 -0700

    Added MainActivity layout with login and signup buttons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,9 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.project2group10real"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.project2group10real"
