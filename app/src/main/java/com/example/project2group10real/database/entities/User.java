a176b0710f1a8f00b7d2962dd2199fe85d931f63
oscar101258 <ogonzalezcuevas@csumb.edu>
2026-04-28 13:41:29 -0700

    Implemented User.java

--- app/src/main/java/com/example/project2group10real/database/entities/User.java
+++ app/src/main/java/com/example/project2group10real/database/entities/User.java
@@ -1,4 +1,73 @@
 package com.example.project2group10real.database.entities;
 
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+import com.example.project2group10real.database.UltimateBudgetingDatabase;
+
+import java.util.Objects;
+@Entity(tableName = UltimateBudgetingDatabase.USER_TABLE)
 public class User {
+    @PrimaryKey(autoGenerate = true)
+    private int user_id;
+    private String username;
+    private String password;
+
+    private boolean isAdmin;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return user_id == user.user_id && isAdmin == user.isAdmin && Objects.equals(username, user.username) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user_id, username, password, isAdmin);
+    }
+
+    public User() {
+    }
+
+    @Ignore
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        isAdmin = false;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
 }
